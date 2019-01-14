59638b8f7fc5bf26bfc752ab383d05daff01952a
glennkroegel <glenn.kroegel@gmail.com>
2019-01-14 01:20:01 +0100

    Added requirements.txt

--- utils.py
+++ utils.py
@@ -38,17 +38,3 @@ def accuracy(input, targs):
     input = input.argmax(dim=-1).view(n,-1)
     targs = targs.view(n,-1)
     return (input==targs).float().mean()
-
-def fbeta(y_pred, y_true, thresh:float=0.2, beta:float=2, eps:float=1e-9, sigmoid:bool=True):
-    "Computes the f_beta between `y_pred` and `y_true` in a multi-classification task."
-    beta2 = beta**2
-    import pdb; pdb.set_trace()
-    if sigmoid: y_pred = y_pred.sigmoid()
-    y_pred = (y_pred>thresh).float()
-    y_true = y_true.float()
-    TP = (y_pred*y_true).sum(dim=1)
-    prec = TP/(y_pred.sum(dim=1)+eps)
-    rec = TP/(y_true.sum(dim=1)+eps)
-    res = (prec*rec)/(prec*beta2+rec+eps)*(1+beta2)
-    return res.mean()
-
